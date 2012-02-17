62da31aba22b898c1fa9e17daebd222ba6c0a617
Alex Fuller <asprowlfuller@yahoo.com>
2012-02-17 08:42:17 -0500

    first commit

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 105);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(13, 381);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(47, 13);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Score: 0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 406);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1{
+    public partial class Form1 : Form{
+        int score=0;
+        int pic = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e){
+            Random random = new Random();
+            pic = random.Next(1 , 7);
+            switch (pic){
+
+                case (1):
+                    button1.BackgroundImage = (System.Drawing.Image)(Properties.Resources.bennett);
+                    break;
+                case (2):
+                    button1.BackgroundImage = (System.Drawing.Image)(Properties.Resources.bennett2);
+                    break;
+                case (3):
+                    button1.BackgroundImage = (System.Drawing.Image)(Properties.Resources.bennett3);
+                    break;
+                case (4):
+                    button1.BackgroundImage = (System.Drawing.Image)(Properties.Resources.bennett4);
+                    break;
+                case (5):
+                    button1.BackgroundImage = (System.Drawing.Image)(Properties.Resources.bennett5);
+                    break;
+                default:
+                    button1.BackgroundImage = (System.Drawing.Image)(Properties.Resources.bennett6);
+                    break;
+            }
+            button1.Size = new Size(button1.BackgroundImage.Width, button1.BackgroundImage.Height);
+            int randomX = random.Next(0, (this.Width-button1.BackgroundImage.Width));
+            int randomY = random.Next(0, (this.Height-button1.BackgroundImage.Height));
+            button1.Location = new Point(randomX, randomY);
+            score++;
+            lbl1.Text = "Score: " + score;
+            timer1.Enabled = true;
+            timer1.Stop();
+            timer1.Start();
+            timer1.Interval -= 50;
+            lbl1.Location = new Point(0, this.Height-50);
+        }
+
+        private void Form1_Load(object sender, EventArgs e){
+        }
+
+        private void timer1_Tick(object sender, EventArgs e){
+            button1.BackgroundImage = (System.Drawing.Image)(Properties.Resources.problem);
+            button1.Size = new Size(button1.BackgroundImage.Width, button1.BackgroundImage.Height);
+            Random random = new Random();
+            int randomX = random.Next(0, (this.Width - button1.BackgroundImage.Width));
+            int randomY = random.Next(0, (this.Height - button1.BackgroundImage.Height));
+            button1.Location = new Point(randomX, randomY);
+            score--;
+            timer1.Interval += 50;
+            lbl1.Text = "Score: " + score;
+        }
+
+
+
+    }
+}
